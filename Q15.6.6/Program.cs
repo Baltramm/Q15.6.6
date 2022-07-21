ac82ffd3d0fbc6dfcf32175bd9f7e19675a7aa2b
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-07-22 03:34:47 +0600

    Start

--- Q15.6.6/Program.cs
+++ Q15.6.6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Q15._6._6
 {
@@ -6,7 +8,25 @@ namespace Q15._6._6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // ???
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
         }
     }
 }
